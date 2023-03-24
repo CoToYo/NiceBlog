078b8d66dea70c0ab65424d3345711f5929762dd
CoToYo <56789038+CoToYo@users.noreply.github.com>
2023-03-24 19:05:19 -0400

    fix storePost

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,7 +1,17 @@
 const Post = require('../database/models/Post')
+const fs = require('fs')
+const path = require('path')
+const imagePath = path.resolve(__dirname, '..', 'public/posts_img')
 
 module.exports = async(req, res)=> {
     try{
+        // clean local storage of image
+        fs.readdir(imagePath, (err, files) => {
+            for(const file of files){
+                fs.unlinkSync(`${imagePath}/${file}`)
+            }
+        })
+
         // find all posts.
         const posts = await Post.find({}).populate('author')
 
--- controllers/storePost.js
+++ controllers/storePost.js
@@ -28,7 +28,7 @@ module.exports = async (req, res) => {
 
         })
 
-        fs.unlinkSync(uploadPath)
+        // fs.unlinkSync(uploadPath)
         res.redirect('/')
 
     } catch(error){
@@ -36,30 +36,4 @@ module.exports = async (req, res) => {
         return res.redirect('/')
     }
     
-        // .then((result) => {
-        //     // storage data
-        //     Post.create({
-        //         ...req.body,
-                
-        //         image: result.secure_url,
-
-        //         // bind the user with this post
-        //         author: req.session.userId
-
-        //     })
-        //     .then((post) => {
-        //         // remove saved image from local
-        //         fs.unlinkSync(uploadPath)
-        //         res.redirect('/')
-        //     })
-        //     .catch((error) => { 
-        //         console.log(error)
-        //         return res.redirect('/') 
-        //     })
-        // })
-        // .catch((error) => {
-        //     console.log(error)
-        //     return res.redirect('/')
-        // })
-
 }
