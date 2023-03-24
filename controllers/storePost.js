606070946ce8726696cb7cdd0da6036e88466912
CoToYo <56789038+CoToYo@users.noreply.github.com>
2023-03-24 17:34:13 -0400

    fix storePost

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -14,31 +14,52 @@ module.exports = async (req, res) => {
     // image.mv(target_path), it moves image to tartget_path
     await image.mv(uploadPath)
 
-    cloudinary.v2.uploader.upload(uploadPath)
-        .then((result) => {
-            // storage data
-            Post.create({
-                ...req.body,
+    try{
+        const result = await cloudinary.v2.uploader.upload(uploadPath)
+
+        // storage data
+        await Post.create({
+            ...req.body,
+            
+            image: result.secure_url,
+
+            // bind the user with this post
+            author: req.session.userId
+
+        })
+
+        fs.unlinkSync(uploadPath)
+        res.redirect('/')
+
+    } catch(error){
+        console.log(`ERROR: ${error}`)
+        return res.redirect('/')
+    }
+    
+        // .then((result) => {
+        //     // storage data
+        //     Post.create({
+        //         ...req.body,
                 
-                image: result.secure_url,
+        //         image: result.secure_url,
 
-                // bind the user with this post
-                author: req.session.userId
+        //         // bind the user with this post
+        //         author: req.session.userId
 
-            })
-            .then((post) => {
-                // remove saved image from local
-                fs.unlinkSync(uploadPath)
-                res.redirect('/')
-            })
-            .catch((error) => { 
-                console.log(error)
-                return res.redirect('/') 
-            })
-        })
-        .catch((error) => {
-            console.log(error)
-            return res.redirect('/')
-        })
+        //     })
+        //     .then((post) => {
+        //         // remove saved image from local
+        //         fs.unlinkSync(uploadPath)
+        //         res.redirect('/')
+        //     })
+        //     .catch((error) => { 
+        //         console.log(error)
+        //         return res.redirect('/') 
+        //     })
+        // })
+        // .catch((error) => {
+        //     console.log(error)
+        //     return res.redirect('/')
+        // })
 
 }
